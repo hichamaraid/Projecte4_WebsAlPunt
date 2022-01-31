a9a57b746912979906ca3cbd8709cc736102d99e
George11849 <george11849@gmail.com>
2022-01-31 19:33:57 +0100

    nose

--- WebApp/digitalearning/resources/views/home.blade.php
+++ WebApp/digitalearning/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('Sessió Correcta!') }}
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
