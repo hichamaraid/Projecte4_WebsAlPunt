19278f98d19a8787503a40c54d4d103f9a3c3ec5
hicham <hichamaraid10@gmail.com>
2022-01-31 17:07:00 +0100

    coses

--- WebApp/digitalearning/app/Providers/RouteServiceProvider.php
+++ WebApp/digitalearning/app/Providers/RouteServiceProvider.php
@@ -38,10 +38,12 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+           
+            Route::prefix('api/v1')
                 ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
+                ->as('api.')
+                ->namespace($this->namespace."\\API")
+                ->group(base_path('routes/api.php'));   
 
             Route::middleware('web')
                 ->namespace($this->namespace)
