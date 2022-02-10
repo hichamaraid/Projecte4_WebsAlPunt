7048e0e851bf43a6f12b5001ac0a176693a90fe3
George11849 <george11849@gmail.com>
2022-02-10 19:33:20 +0100

    Login

---
+++ WebApp/digitalearning/resources/views/Footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>DigitalEarning </strong> by <a href="https://github.com/hichamaraid">Hicham & </a> <a href="https://github.com/George11849">George</a>. 
+      <strong> &copy; Copyright  2022 by DigitalEarning. All Rights Reserved. </strong>
+    </p>
+  </div>
+</footer>
--- WebApp/digitalearning/resources/views/welcome.blade.php
+++ WebApp/digitalearning/resources/views/welcome.blade.php
@@ -1,137 +1,95 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <title>Laravel George</title>
-        <link rel="stylesheet" href="css/app.css">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>DIGITALEARNING</title>
+  </head>
+  <body id="fondo-home">
+  @include('Nav')
+<div class="img-home">
+    <div class="text-img-homee">
+        <p class="text-stilo"><strong>Aprendre Interessadament</strong></p>
+        <p class="text-sub-stilo"><strong>Aprèn per part teva, formar-te i poder ser el millor.</strong></p>
+        <img src="{{ asset('Images/libro.png')}}" alt="fdf" id="foto-home">
+    </div>
+</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway';
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-            .full-height {
-                height: 100vh;
-            }
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-            .position-ref {
-                position: relative;
-            }
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-            .content {
-                text-align: center;
-            }
-            .title {
-                font-size: 84px;
-            }
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-    </a>
-
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
-
-      <a class="navbar-item">
-        Documentation
-      </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
+<div class="flex-home">
+    <div class="flex-one">
+      <h1 class="titulos"><strong>DigitalEarning</strong></h1>
+      <p class="text-home">Vols continuar formant-te i continuar aprenent cada dia més i més? Ets al lloc perfecte per millorar les teves habilitats i coneixements.</p>
     </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </div>
+    <div class="flex-two">
+      <img src="{{ asset('Images/aprender.png')}}" alt="foto" class="fotos-home">
     </div>
+</div>
+
+<div class="flex-home-two">
+    <div class="flex-three">
+      <img src="{{ asset('Images/persona-estudiando.png')}}" alt="foto" class="fotos-home-two">
+    </div>
+
+    <div class="flex-four">
+      <h1 class="titulos"><strong> Transforma la teva vida mitjançant els estudis per ser el millor del món!</strong></h1>
+      <p class="text-home">Persones que volen aprendre, com tu, volen tenir una nova aventura estudiant.</p>
+    </div>
+</div>
+
+<div class="linea-iconos">
+  <img src="{{ asset('Images/piton.png')}}" alt="foto" class="iconos-img">
+  <img src="{{ asset('Images/js.png')}}" alt="foto" class="iconos-img">
+  <img src="{{ asset('Images/php.png')}}" alt="foto" class="iconos-img">
+  <img src="{{ asset('Images/java.png')}}" alt="foto" class="iconos-img">
+  <img src="{{ asset('Images/mysql.png')}}" alt="foto" class="iconos-img">
+  <img src="{{ asset('Images/tres-puntos.png')}}" alt="foto" class="iconos-img">
+</div>
+
+
+<div class="scroll-horizontal">
+  <div>
+    <img src="{{ asset('Images/bulma-scroll.png')}}" alt="foto" class="iconos-img">
   </div>
-</nav>
-        <div class="flex-center position-ref full-height">
+  <div>
+    <img src="{{ asset('Images/sql-scroll.png')}}" alt="foto" class="iconos-img">
+  </div>
+  <div>
+    <img src="{{ asset('Images/java-scroll.png')}}" alt="foto" class="iconos-img">
+  </div>
+  <div>
+    <img src="{{ asset('Images/php-scroll.png')}}" alt="foto" class="iconos-img">
+  </div>
+  <div>
+    <img src="{{ asset('Images/laravel-scroll.png')}}" alt="foto" class="iconos-img">
+  </div>
+  <div>
+    <img src="{{ asset('Images/vue-scroll.png')}}" alt="foto" class="iconos-img">
+  </div>
 
-            @if (Route::has('login') && Auth::check())
-                <div class="top-right links">
-                    <a href="{{ url('/home') }}">Dashboard</a>
-                </div>
-            @elseif (Route::has('login') && !Auth::check())
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-             
-            </div>
-         
+</div>
 
-    </body>
+
+
+
+
+
+
+
+  @include('Footer')
+  </body>
 </html>
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+         
