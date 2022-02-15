f070beff431667817e56e35d9a00ec4d026192bf
hicham <hichamaraid10@gmail.com>
2022-02-15 18:44:27 +0100

    xd

--- WebApp/digitalearning/routes/api.php
+++ WebApp/digitalearning/routes/api.php
@@ -18,11 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();    
 });
 
-Route::get('/anime',function(){
-    return Anime::all();
-}
 
-);
 
 
 
