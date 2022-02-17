bc4e916657a25e31bb2e41c8dd3f507ffdf75701
George11849 <george11849@gmail.com>
2022-02-17 18:07:28 +0100

    Eliminacion

--- WebApp/digitalearning/resources/js/app.js
+++
@@ -1,7 +0,0 @@
-require('./bootstrap');
-
-import { createApp } from 'vue';
-import welcome from './components/welcome.vue';
-const app = createApp({});
-app.component('welcome', welcome);
-app.mount('#app');
